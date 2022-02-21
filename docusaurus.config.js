4cd9c4350b6c7384391dc96cbe1b08d7de71e5dc
gtsui <tsui.geoffrey@gmail.com>
2022-02-21 21:25:47 +0800

    update url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Qoda Finance',
   tagline: '',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://qoda.netlify.app',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
