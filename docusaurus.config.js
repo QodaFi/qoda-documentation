06079545100e74359bc563b6f34d0ea3c7452913
gtsui <tsui.geoffrey@gmail.com>
2022-02-21 21:31:10 +0800

    fix broken link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -75,7 +75,7 @@ const config = {
             items: [
               {
                 label: 'Documentation',
-                to: '/docs/intro',
+                to: '/',
               },
             ],
           },
